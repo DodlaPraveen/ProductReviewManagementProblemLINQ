bc221e498d11101f892d2f4f20d4908f17f25a8a
praveen dodla <dodlapraveen09@gmail.com>
2022-09-29 19:51:42 +0530

    show top 3 records

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review managament program ");
+
+            ProductManagement pre = new ProductManagement();
+        }
+    }
+}
